b0e8f56073c7e984808b09bbc136272c266ba2a6
Kaviya.A <Kaviya.A@kaviya>
2025-08-22 21:00:26 +0530

    day 9 completed

---
+++ C2TC_javacoreprograms/java_project/src/com/tnsif/daynine/StringSubClass.java
@@ -0,0 +1,30 @@
+ //can't inherit final classes
+package com.tnsif.daynine;
+
+public class StringSubClass {
+    private String value;
+
+    public StringSubClass(String value) {
+        this.value = value;
+    }
+
+    public int length() {
+        return value.length();
+    }
+
+    public String toUpperCase() {
+        return value.toUpperCase();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        StringSubClass myStr = new StringSubClass("Hello World");
+        System.out.println("Length: " + myStr.length());
+        System.out.println("Uppercase: " + myStr.toUpperCase());
+        System.out.println("Original: " + myStr);
+    }
+}
